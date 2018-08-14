27714a35820d7c82a2e937b7107a0ff7402ee88f
Stan <potomak123@googlemail.com>
2018-08-14 14:35:56 +0200

    Naprawiono połączenie z bazą danych

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:stasiu84@ds121182.mlab.com:21182/todoapi'
+};
--- models/index.js
+++ models/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api')
+
+// Load Keys
+const keys = require('../config/keys');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 
 mongoose.Promise = Promise; // pozwala na używanie syntaksu z promise
 
