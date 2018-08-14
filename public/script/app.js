dbe2e24854779130c4b3d7fda084f6b8c8455738
Stan <potomak123@googlemail.com>
2018-08-14 17:56:08 +0200

    Writing Initial AJAX Call

--- public/script/app.js
+++ public/script/app.js
@@ -1,1 +1,14 @@
-alert('Hi from app.js')
+$(document).ready(() => { // uruchomi się dopiero jak cały DOM zostanie załadowany
+  $.getJSON('/api/todos')
+    .then(addTodos)
+    .catch(err => console.log(err))
+});
+
+const addTodos = (todos) => {
+  // add todos to the page
+  todos.forEach(todo => {
+    const newTodo = $('<li class="task">' + todo.name + '</li>');  // class można dodać w ten sposób lub  newTodo.addClass('task')
+    // newTodo.addClass('task')
+    $('.list').append(newTodo)
+  })
+}
