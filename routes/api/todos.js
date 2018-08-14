d0dc766cbb3bf41995ecc2c0c611b031ccd03571
Stan <potomak123@googlemail.com>
2018-08-14 16:42:14 +0200

    Refactoring Our API

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,55 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../models');
+const helpers = require('../../helpers/todos')
 
 // @route   GET api/todos
 // @desc    Show all todos
 // @access  Public
-router.get('/', (req, res) => {
-  db.Todo.find()
-    .then(todos => res.json(todos))
-    .catch(err => res.send(err))
-});
-
+// ****************************
+// łączymy razem routes o takiej samej ścieżce
+// ****************************
 // @route   POST api/todos
 // @desc    Create todo
 // @access  Public
-router.post('/', (req, res) => {
-  db.Todo.create(req.body)
-    .then(newTodo => {
-      res.status(201).json(newTodo)
-    })
-    .catch(err => res.send(err))
-});
+router.route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo)
+
 
 // @route   GET api/todos/:todoId
 // @desc    Show todo
 // @access  Public
-router.get('/:todoId', (req, res) => {
-  db.Todo.findById(req.params.todoId)
-    .then(foundTodo => res.json(foundTodo))
-    .catch(err => res.send(err))
-});
-
+// ****************************
 // @route   PUT api/todos/:todoId
 // @desc    UPDATE
 // @access  Public
-router.put('/:todoId', (req, res) => {
-  db.Todo.findOneAndUpdate({ _id: req.params.todoId }, req.body, { new: true })  // new: true zwraca nową wartość po update
-    .then(todo => {
-      res.json(todo)
-    })
-    .catch(err => res.send(err))
-});
-
+// ****************************
 // @route   DELETE api/todos/:todoId
 // @desc    Remove
 // @access  Public
-router.delete('/:todoId', (req, res) => {
-  db.Todo.remove({ _id: req.params.todoId })
-    .then(() => res.json({ message: 'We deleted it!' }))
-    .catch(err => res.send(err))
-});
+router.route('/:todoId')
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
 
 
 module.exports = router;
